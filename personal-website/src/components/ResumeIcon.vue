f520da9cc6f8190128dc6fdebd52bf12ab15ce1f
Logan Hochwald <loganh42@gmail.com>
2024-02-18 18:19:38 -0600

    FEAT!: create quasar project

---
+++ personal-website/src/components/ResumeIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <q-btn
+      size="35px"
+      color="teal"
+      icon="description"
+    />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ResumeIcon',
+});
+</script>
---
+++ personal-website/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <ResumeIcon />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import ResumeIcon from 'src/components/ResumeIcon.vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {
+    ResumeIcon,
+  },
+});
+</script>
